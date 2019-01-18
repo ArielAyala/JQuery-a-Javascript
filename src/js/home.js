bfc24f59ccdc8340073b39a78bfa471d432429ca
Ariel <arielayalamail@gmail.com>
2019-01-18 07:35:52 -0300

    Iniciando función findMovie

--- src/js/home.js
+++ src/js/home.js
@@ -189,11 +189,16 @@ fetch('https://randomuser.me/api/dsfdsfsd')
   const $modalImage = $modal.querySelector('img');
   const $modalDescription = $modal.querySelector('p');
 
+  function findMovie(id,category) {
+    
+  }
+
   function showModal($element) {
     $overlay.classList.add('active');
     $modal.style.animation = 'modalIn .8s forwards';
     const id = $element.dataset.id
     const category = $element.dataset.category;
+    const data = findMovie(id,category);
   }
 
   $hideModal.addEventListener('click', hideModal);
